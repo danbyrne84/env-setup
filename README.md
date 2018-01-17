6d3700731d6ea55628df7086bb51882952b2a432
Dan Byrne <dan.byrne@vianetplc.com>
2018-01-17 16:12:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Basic environment setup scripts
+
+Run: .\install.bat from a command prompt with administrative rights
+
---
+++ install.bat
@@ -0,0 +1,29 @@
+echo off
+
+echo 1 * Installing package manager: chocolatey
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo 2 * Installing editors: Visual Studio Code, Sublime Text 3
+
+@cinst -y VisualStudioCode sublimetext3 --limitedoutput
+
+echo 3 * Installing extras: cmder, git, node
+
+@cinst -y cmder git nodejs --limitoutput
+
+echo 4 * Install Angular CLI
+
+# call npm install -g "@angular/cli" --silent
+
+echo 5 * Checking out demo project from source control
+
+mkdir Project
+
+@git clone "https://github.com/danbyrne84/dominion-game.git" Project
+
+echo 6 Launching VS Code with demo project
+
+code .\Project
+
+echo Done
